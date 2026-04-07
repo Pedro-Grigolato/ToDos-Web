ecc68239f9c0871c9b93eeaa4c64de03cb8b8386
Pedro-grigolato <gzpedrao16@gmail.com>
2026-04-07 12:02:12 -0300

    feat: adiciona estrutura base do projeto com login, layout e dashboard

--- ToDoPlatform/wwwroot/js/site.js
+++ ToDoPlatform/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const userMenu = document.getElementById('userMenu');
+const dropdown = document.getElementById('dropdown');
+
+userMenu.addEventListener('click', (e) => {
+    e.stopPropagation();
+    dropdown.classList.toggle('activate');
+});
+
+document.addEventListener('click', () => {
+    dropdown.classList.remove('activate');
+});
